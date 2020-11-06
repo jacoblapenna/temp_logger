3bc67711cf378c314e865f47b8ab8195ccf37509
Jacob Lapenna <jacoblapenna@gmail.com>
2020-11-05 21:34:23 -0700

    Average temperatures

--- garage_temp_app.py
+++ garage_temp_app.py
@@ -6,4 +6,9 @@ cur = conn.cursor()
 
 sense = SenseHat()
 
-print(sense.get_temperature_from_humidity(), sense.get_temperature_from_pressure())
+def get_temp():
+    h_temp = sense.get_temperature_from_humidity()
+    p_temp = sense.get_temperature_from_pressure()
+    return (h_temp + p_temp) / 2
+    
+print(get_temp())
