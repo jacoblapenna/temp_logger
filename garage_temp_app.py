473e38da0074468829b2e794e5f2dc24af6e73e5
Jacob Lapenna <jacoblapenna@gmail.com>
2020-11-09 09:50:32 -0700

    Added socketio for main to run app

--- garage_temp_app.py
+++ garage_temp_app.py
@@ -4,10 +4,12 @@ from datetime import datetime as dt
 import matplotlib.pyplot as plt
 from sense_hat import SenseHat
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 import psycopg2 as db
 import time
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 conn = db.connect("dbname=garage_temps")
 cur = conn.cursor()
@@ -16,6 +18,20 @@ table = "test"
 cur.execute(f"CREATE TABLE IF NOT EXISTS {table} (time float, temp float);")
 
 sense = SenseHat()
+
+def get_ip_address():
+    """ get the server's ip """
+
+    ip_address = '127.0.0.1'  # Default to localhost
+    s = sock.socket(sock.AF_INET, sock.SOCK_DGRAM) # setup a socket object
+
+    try:
+        s.connect(('1.1.1.1', 1))  # does not have to be reachable
+        ip_address = s.getsockname()[0]
+    finally:
+	    s.close()
+
+    return ip_address
 
 def get_temp():
     """ get temp from sensors, averages, and converts to Fahrenheit """
@@ -54,6 +70,16 @@ def record_data():
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    ip = get_ip_address()
+    print("Attempting to serve page on %s:%d" % (ip, 8080))
+    socketio.run(app,
+                 host=ip,
+                 port=8080,
+                 use_reloader=True,
+                 debug=False,
+                 extra_files=['templates/index.html'])
 
 """
 One child process to log data
