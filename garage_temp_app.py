66e221f6fd2c03f5ba94d54c0a0fe55bd5ab6fc5
Jacob Lapenna <jacoblapenna@gmail.com>
2020-11-08 21:17:21 -0700

    Loop insert into db

--- garage_temp_app.py
+++ garage_temp_app.py
@@ -1,16 +1,44 @@
 from sense_hat import SenseHat
 import psycopg2 as db
+from datetime import datetime as dt
+import time
 
 conn = db.connect("dbname=garage_temps")
 cur = conn.cursor()
 
+table = "test"
+cur.execute(f"CREATE TABLE {table} (time float, temp float);")
+
 sense = SenseHat()
 
 def get_temp():
+    """ get temp from sensors, averages, and converts to Fahrenheit """
 
+    # get temp from humidity sensor
     h_temp = sense.get_temperature_from_humidity()
+    # get te,p from pressure sensor
     p_temp = sense.get_temperature_from_pressure()
 
+    # get average of two sensors, convert, and return
     return round(((h_temp + p_temp) / 2)*(9/5) + 32, 2)
 
-print(get_temp())
+def insert_temp():
+    """ get's current temp and time and inserts into database """
+
+    # get temp data and present time
+    temp = get_temp()
+    time = time.time()
+
+    # insert new temp with time of temp data
+    cur.execute(f"INSERT INTO {table} (year, temp) VALUES ({time}, {temp});")
+
+    # commit entry to database
+    conn.commit()
+
+
+while True:
+    try:
+        insert_temp()
+        time.sleep(1)
+    except KeyboardInterrupt:
+        break
