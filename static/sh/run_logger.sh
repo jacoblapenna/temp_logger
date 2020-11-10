57e0081dac51e8b59da0afe463e4f518b5ba8461
Jacob Lapenna <jacoblapenna@gmail.com>
2020-11-10 09:21:51 -0700

    debug

--- static/sh/run_logger.sh
+++ static/sh/run_logger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/pi/Projects/Garage_Temp_Logger/gt_env/bin/activate
-python3 /home/pi/Projects/Garage_Temp_Logger/static/py/temp_logger.py
+source "/home/pi/Projects/Garage_Temp_Logger/gt_env/bin/activate"
+python "/home/pi/Projects/Garage_Temp_Logger/static/py/temp_logger.py"
